588ddc52ab84979e8fdb53f7f3a1005abee178b0
yukikaburi123 <guspokemon5@gmail.com>
2026-05-04 23:58:13 -0400

    fix paths and config for github pages

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '',
+  base: '/zbll-trainer/',
   plugins: [vue()],
   resolve: {
     alias: {
